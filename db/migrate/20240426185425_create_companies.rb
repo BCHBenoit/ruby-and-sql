cc3be1ca91c7c0e117a9c5759f845a4032c583e5
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-04-26 20:47:35 +0000

    guardado3

--- db/migrate/20240426185425_create_companies.rb
+++ db/migrate/20240426185425_create_companies.rb
@@ -1,10 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration[7.1]
   def change
     create_table :companies do |t|
-      t.string "name"
-      t.string "city"
-      t.string "state"
-      t.string "url"
+
 
       t.timestamps
     end
